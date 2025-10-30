b8c37e3a2e69a483a214dc86fbcc03ee9c2ce64b
Thaís <tscampos31@gmail.com>
2025-10-30 10:30:14 -0300

    Aula sobre laços condicionais

---
+++ aula02/ExemploIfElse.js
@@ -0,0 +1,21 @@
+const leia = require('readline-sync')
+
+let nota1, nota2, media
+
+nota1 = leia.questionFloat("Digite a primeira nota: ")
+
+nota2 = leia.questionFloat("Digite a segunda nota: ")
+
+//Processamento
+media = (nota1 + nota2) / 2
+
+// = -> atribuição
+// == -> igualdade | 5 == 5
+
+if (media >= 6){
+    console.log("Parabéns, vc passou!")
+} else if(media == 5){
+    console.log("Vc está de exame!")
+}else{
+    console.log("Vc reprovou!")
+}
