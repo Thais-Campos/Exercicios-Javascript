1aa29a58337f5223a2ac2efde057080d110bc981
Thaís <tscampos31@gmail.com>
2025-11-04 10:11:26 -0300

    Aula sobre fila e pilha

--- aula03/ExemploDoWhile.js
+++ aula03/ExemploDoWhile.js
@@ -3,7 +3,6 @@ const leia = require("readline-sync");
 let numero, resultado, contador = 3;
 
 do {
-    
     numero = leia.questionInt("\nDigite um numero inteiro: ");
 
     resultado = numero * 5;
